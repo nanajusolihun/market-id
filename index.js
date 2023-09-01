c5531b7f7a1a471f2f8b34a41eb101b05964c1e5
Nana Jusolihun <nanajusolihun@gmail.com>
2023-09-01 09:21:33 +0700

    feature: allow cors

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ import r_Address from "./src/routers/r_address.js";
 
 const app = express();
 
-app.use(cors({origin: `http://localhost:${PORT}`}));
+app.use(cors());
+// app.use(cors({origin: `http://localhost:${PORT}`}));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
